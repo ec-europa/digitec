2eecb93e7fd085e8db288a41593020be4e38483d
Yannick Huard <mail@yannickhuard.com>
2018-05-28 14:42:44 +0200

    feat(practical): bootstrap practical page (#15)

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,18 +1,14 @@
 import CMS from 'netlify-cms';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview';
-import BlogPostPreview from './preview-templates/BlogPostPreview';
 import HomePagePreview from './preview-templates/HomePagePreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
+import PracticalPagePreview from './preview-templates/PracticalPagePreview';
 import SpeakerPreview from './preview-templates/SpeakerPreview';
 import EventPreview from './preview-templates/EventPreview';
 import StandPreview from './preview-templates/StandPreview';
 
 CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('homepage', HomePagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('practical', PracticalPagePreview);
 CMS.registerPreviewTemplate('speakers', SpeakerPreview);
 CMS.registerPreviewTemplate('events', EventPreview);
 CMS.registerPreviewTemplate('stands', StandPreview);
