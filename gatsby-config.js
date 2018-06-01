f58fd685d3cd52e6389f6c46895412bb5c90dc6a
Yannick Huard <mail@yannickhuard.com>
2018-06-01 17:44:13 +0200

    feat(pwa): make it a pwa (#33)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,27 @@
+const config = require('./config.json');
+
 module.exports = {
   siteMetadata: {
-    title: 'DIGITEC',
+    title: config.siteTitle,
+    description: config.siteDescription,
+    siteUrl: config.siteUrl,
+    pathPrefix: config.pathPrefix,
   },
   plugins: [
+    `gatsby-plugin-react-next`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'DIGITEC 2018',
+        short_name: 'DIGITEC',
+        start_url: '/',
+        background_color: '#fcfdfd',
+        theme_color: '#009ee2',
+        display: 'minimal-ui',
+        icon: 'src/img/logo-small.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
@@ -41,6 +60,9 @@ module.exports = {
       options: {
         plugins: [],
       },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
