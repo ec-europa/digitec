2189b2ec73679d5d5a3e0c72acb1d4662676f83d
Kalin Chernev <kalin.chernev@gmail.com>
2018-07-16 09:21:54 +0200

    Small corrections

--- src/components/Presenters/Team/Row.jsx
+++ src/components/Presenters/Team/Row.jsx
@@ -36,7 +36,7 @@ const Row = ({ team }) => (
     <div className={styles.presenterInfo}>
       <h3>{team.teamName}</h3>
       <h4 className={styles.title}>{team.intro}</h4>
-      <p>{team.teamMembers}</p>
+      <p className={styles.presenterParagraph}>{team.teamMembers}</p>
     </div>
   </Link>
 );
