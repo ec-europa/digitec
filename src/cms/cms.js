4da0061e590e12834299a989a838421e3a98e77d
Kalin Chernev <kalin.chernev@gmail.com>
2020-03-19 13:59:05 +0100

    Refactor netlify cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,12 +7,9 @@ import TeamPreview from './preview-templates/TeamPreview';
 import EventPreview from './preview-templates/EventPreview';
 import StandPreview from './preview-templates/StandPreview';
 
-CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('homepage', HomePagePreview);
 CMS.registerPreviewTemplate('practical', PracticalPagePreview);
 CMS.registerPreviewTemplate('speakers', SpeakerPreview);
 CMS.registerPreviewTemplate('teams', TeamPreview);
 CMS.registerPreviewTemplate('events', EventPreview);
 CMS.registerPreviewTemplate('stands', StandPreview);
-
-CMS.init();
