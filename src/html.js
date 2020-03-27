8e562b33dd939e2d6073b2efd3e3aa5a4208b470
Kalin Chernev <kalin.chernev@gmail.com>
2020-03-27 10:52:50 +0100

    Refactor html.js

--- src/html.js
+++ src/html.js
@@ -1,63 +1,62 @@
-/* eslint-disable global-require,react/prefer-stateless-function, react/jsx-filename-extension, jsx-a11y/html-has-lang,react/prop-types,no-useless-escape */
+/* eslint-disable global-require, react/jsx-filename-extension, react/prop-types */
 const React = require('react');
 
-module.exports = class HTML extends React.Component {
-  render() {
-    let css;
-    const {
-      htmlAttributes,
-      headComponents,
-      bodyAttributes,
-      preBodyComponents,
-      postBodyComponents,
-      body,
-    } = this.props;
+const html = ({
+  htmlAttributes,
+  headComponents,
+  bodyAttributes,
+  preBodyComponents,
+  postBodyComponents,
+  body,
+}) => {
+  let css;
 
-    return (
-      <html {...htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          {headComponents}
-          {css}
-          <script
-            src="https://ec.europa.eu/wel/cookie-consent/consent.js"
-            type="text/javascript"
-            defer
-          />
-        </head>
-        <body {...bodyAttributes}>
-          {preBodyComponents}
-          <div
-            key="body"
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: body }}
-          />
-          {postBodyComponents}
-          <script
-            defer
-            src="//europa.eu/webtools/load.js"
-            type="text/javascript"
-          />
-          <script
-            type="application/json"
-            dangerouslySetInnerHTML={{
-              __html: `
+  return (
+    <html lang="en" {...htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {headComponents}
+        {css}
+        <script
+          src="https://ec.europa.eu/wel/cookie-consent/consent.js"
+          type="text/javascript"
+          defer
+        />
+      </head>
+      <body {...bodyAttributes}>
+        {preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
+        {postBodyComponents}
+        <script
+          defer
+          src="//europa.eu/webtools/load.js"
+          type="text/javascript"
+        />
+        <script
+          type="application/json"
+          dangerouslySetInnerHTML={{
+            __html: `
               {
                   "utility": "piwik",
                   "siteID": 101,
-                  "sitePath": ["europa.eu\/digitec/2018"],
+                  "sitePath": ["europa.eu/digitec/2018"],
                   "instance": "europa.eu"
               }
             `,
-            }}
-          />
-        </body>
-      </html>
-    );
-  }
+          }}
+        />
+      </body>
+    </html>
+  );
 };
+
+module.exports = html;
